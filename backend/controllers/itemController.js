f2d862d2cb514e65b2740cf516a8d9be484f26a8
s0560996 <ododmengapa@gmail.com>
2024-06-05 16:42:35 +0200

    Added controller

---
+++ backend/controllers/itemController.js
@@ -0,0 +1,105 @@
+const Item = require('../models/itemsModel')
+const mongoose = require('mongoose')
+
+// get all items
+const getItems = async (req, res) => {
+    const items = await Item.find({}).sort({ createdAt: -1 }) //Empty .find property{}, to get all items 
+
+    res.status(200).json(items)
+}
+
+// get a single item
+
+const getItem = async (req, res) => {
+    // grab id from req parameter :id
+    const { id } = req.params
+
+    //check if id is valid type
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such item' })
+    }
+
+    const item = await Item.findById(id)
+
+    if (!item) {
+        return res.status(404).json({ error: 'No such item' })
+    }
+
+    res.status(200).json(item)
+}
+// create a new item
+const createItem = async (req, res) => {
+    // grabbing properties from req.body
+    const { title, desc, load } = req.body
+
+    // add doc to db
+    try {
+        const item = await Item.create({ title, desc, load }) //Async, create new item document
+        res.status(200).json(item) //return it as json
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+// delete an item
+const deleteItem = async (req, res) => {
+    // grabbing id
+    const { id } = req.params
+
+    //check if id is valid
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: "No such item" })
+    }
+    // check if it exist
+    const item = await Item.findById(id)
+
+    if (!item) {
+        return res.status(404).json({ error: 'No such item' })
+    }
+
+    try {
+        const item = await Item.findByIdAndDelete(id)
+        res.status(200).json({ deleted: item })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+// update an item
+const updateItem = async (req, res) => {
+    // grabbing property
+    const { title, desc, load } = req.body
+    // grabbing id
+    const { id } = req.params
+
+    //check if id is valid
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such item' })
+    }
+    // check if it exist
+    const item = await Item.findById(id)
+
+    if (!item) {
+        return res.status(404).json({ error: 'No such item' })
+    }
+
+    try {
+        const item = await Item.findByIdAndUpdate(id, {
+            //Spread the object
+            ...req.body 
+        })
+
+        res.status(200).json({updated: item})
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+// export as object
+module.exports = {
+    createItem,
+    getItems,
+    getItem,
+    deleteItem,
+    updateItem
+}
--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,36 +1,27 @@
 const express = require('express')
-const Item = require('../models/itemsModel')
+const {
+    createItem,
+    getItems,
+    getItem,
+    deleteItem,
+    updateItem
+} = require('../controllers/itemController')
+
 const router = express.Router()
+//This file is for register the routes, no controller function
 
 //GET all items '/api/items'
-router.get('/', (req, res) => {
-    res.json({ mssg: 'GET all workout' })
-})
+router.get('/', getItems)
 
 //GET single item
-router.get('/:id', (req, res) => {
-    res.json({ mssg: 'GET single workout' })
-})
+router.get('/:id', getItem)
 
 // POST a new item
-router.post('/', async (req, res) => {
-    const { title, desc, load } = req.body
-
-    try {
-        const item = await Item.create({ title, desc, load }) //Async, create new item document
-        res.status(200).json(item)
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-})
+router.post('/', createItem)
 
 // DELETE an item
-router.delete('/:id', (req, res) => {
-    res.json({ mssg: 'DELETE an item' })
-})
+router.delete('/:id', deleteItem)
 
 // UPDATE an item
-router.patch('/:id', (req, res) => {
-    res.json({ mssg: 'UPDATE an item' })
-})
+router.patch('/:id', updateItem)
 module.exports = router
